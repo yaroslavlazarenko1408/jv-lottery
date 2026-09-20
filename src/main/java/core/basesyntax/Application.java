46445fbec4dd11f600858d8b11a6d2779b091019
Yaroslav Lazarenko <yaroslavlazarenko1408@gmail.com>
2026-09-20 18:23:34 +0200

    New changes after checkstyle problems

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,11 +1,14 @@
 package core.basesyntax;
+
 public class Application {
     public static void main(String[] args) {
         Ball[] balls = new Ball[3];
         Lottery lottery = new Lottery();
+
         for (int i = 0; i < 3; i++) {
             balls[i] = lottery.getRandomBall();
         }
+
         for (Ball ball : balls) {
             System.out.println(ball.toString());
         }
